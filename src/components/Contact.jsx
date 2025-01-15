4612b945cd77f7608a76501282f23998bd3c869b
WebApp07 <aminezouguig@proton.me>
2025-01-15 18:11:49 +0100

    feature3 section res

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -39,7 +39,7 @@ const Contact = () => {
           </p>
 
           <AnimatedTitle
-            title="let&#39;s b<b>u</b>ild the <br /> new era of <br /> Security <b>in</b> <b>system</b> together."
+            title="let&#39;s b<b>u</b>ild the <br /> new era of <br /> Security <b>System</b> <b>system</b> together."
             className="special-font !md:text-[6.2rem] w-full font-zentry !text-5xl !font-black !leading-[.9]"
           />
 
